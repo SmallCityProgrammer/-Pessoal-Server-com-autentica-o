e9c3832442e86abb7a403fd1477053b14a6927aa
Eliezer Castro <smallcity.programmer@gmail.com>
2023-11-19 16:18:52 -0300

    metodo put anterior deletado

--- Routes/indexRoute.js
+++ Routes/indexRoute.js
@@ -6,7 +6,7 @@ const authService = require("../Service/authService")
 router.get('/', homeController.get)
 router.get('/:id', homeController.getById)
 router.post('/', authService.authorize, homeController.post)
-router.put('/:id', authService.authorize, homeController.put)
+router.put('/:id', homeController.put)
 router.delete('/:id', homeController.delete)
 
 module.exports = router;
