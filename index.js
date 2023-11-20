fcd21682b106dbafa1b56ef8712b57331c0be029
Eliezer Castro <smallcity.programmer@gmail.com>
2023-11-20 00:07:27 -0300

    deploy

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended:true}))
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', './views');
 app.set('view engine', 'ejs')
 
 //Views
