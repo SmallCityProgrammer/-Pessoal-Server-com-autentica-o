d2340c95416d4babd81af324ad71673c46a14063
Eliezer Castro <smallcity.programmer@gmail.com>
2023-11-21 22:09:00 -0300

    deploy

--- Routes/indexRoute.js
+++ Routes/indexRoute.js
@@ -5,8 +5,8 @@ const authService = require("../Service/authService")
 
 router.get('/', homeController.get)
 router.get('/:id', homeController.getById)
-router.post('/', authService.authorize, homeController.post)
+router.post('/', homeController.post)
 router.put('/:id', homeController.put)
-router.delete('/:id', homeController.delete)
+router.delete('/:id',authService.authorize, homeController.delete)
 
 module.exports = router;
--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", __dirname + "/views");
 app.set('view engine', 'ejs');
+
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'Public')));
