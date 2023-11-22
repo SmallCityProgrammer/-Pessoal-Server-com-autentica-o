46fd333d3619b947a5a81acb542e8d66c1065129
Eliezer Castro <smallcity.programmer@gmail.com>
2023-11-21 22:14:54 -0300

    deploy

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.set("views", __dirname + "/views");
+app.set("views", __dirname + "views");
 app.set('view engine', 'ejs');
 
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'Public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended:true}))
 
 module.exports = app;
